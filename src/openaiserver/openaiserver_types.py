f4cce825da70c69a693b342b9d3d4ef7eaa97e63
Martin Chapman <contact@martinchapman.co.uk>
2026-06-14 11:23:32 +0100

    chore: add max_tokens alias

--- src/openaiserver/openaiserver_types.py
+++ src/openaiserver/openaiserver_types.py
@@ -4,11 +4,13 @@ from openai import BaseModel
 from openai.types.chat import ChatCompletionMessageParam
 from openai.types.chat_model import ChatModel
 
+from pydantic import Field
+
 
 class ChatCompletionRequest(BaseModel):
     model: ChatModel | str
     messages: list[ChatCompletionMessageParam]
-    max_tokens: int | None
+    max_tokens: int | None = Field(..., alias='max_completion_tokens')
     temperature: float | None
 
 
